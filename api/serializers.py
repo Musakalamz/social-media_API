f1c2e4ab16aa4312ef7c15ddf7e896eced190699
Musakalamz <ogunsolumusa@gmail.com>
2025-12-20 23:26:50 +0100

    feat: Create serializers for User, Post, and Follow models

---
+++ api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Post, Follow
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'content', 'image', 'video', 'audio', 'created_at']
+
+class FollowSerializer(serializers.ModelSerializer):
+    follower = UserSerializer(read_only=True)
+    following = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Follow
+        fields = ['id', 'follower', 'following', 'created_at']
