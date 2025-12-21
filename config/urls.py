26032a6357e2ffff63f17db8eac1bf00ccc89491
Musakalamz <ogunsolumusa@gmail.com>
2025-12-21 16:14:11 +0100

    feat: Add Swagger/OpenAPI documentation

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,30 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from api.views import root_redirect
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Social Media API",
+      default_version='v1',
+      description="API documentation for Social Media Platform",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@social.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', root_redirect),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
